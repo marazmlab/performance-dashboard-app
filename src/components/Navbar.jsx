1671078e778b96858a67d3583f3e6287719bf49f
Marek Bełz <marazm.lab@gmail.com>
2025-07-18 15:28:21 +0000

    feat: logo added, and integrated in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,24 @@
 import { Link } from "react-router-dom";
 
 import ThemeToggle from "./ThemeToggle";
+import Logo from '../assets/logo.svg?react';
 
 function Navbar() {
-    return (
-        <nav className="bg-gray-800  dark:bg-white p-4 w-full">
-            <div className="flex flex-col sm:flex-row items-center justify-between gap-4">
-                <div className="container mx-auto flex items-center justify-between">
-                    <Link to="/" className="text-white dark:text-gray-800 hover:underline">Performance Audit Dashboard</Link>
-                    <ThemeToggle />
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="bg-gray-800  dark:bg-white p-4 w-full">
+      <div className="flex flex-col sm:flex-row items-center justify-between gap-4">
+        <div className="container mx-auto flex items-center justify-between">
+          <Link to="/" className="flex items-center gap-3 group">
+            <Logo className="w-6 h-6 dark:text-gray-800 text-white transition-colors" />
+            <span className="text-lg font-bold dark:text-gray-800 text-white group-hover:underline">
+              Performance Audit Dashboard
+            </span>
+          </Link>
+          <ThemeToggle />
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
